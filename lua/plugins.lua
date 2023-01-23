3f056374821d02c8d718f6a7ee5d2795309c15ba
alemi <me@alemi.dev>
2023-01-23 22:01:11 +0100

    fix: jdtls cleanup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -288,16 +288,8 @@ local init_fn = function(use)
 	lspconfig.pylsp.setup({capabilites = capabilities, on_attach = set_lsp_binds, settings = { pylsp = { plugins = { pycodestyle = { enabled = false } } } } })
 	lspconfig.clangd.setup({capabilities=capabilities, on_attach=set_lsp_binds})
 	lspconfig.ltex.setup({capabilities=capabilities, on_attach=set_lsp_binds})
-
-	local jdtls_bin_path = os.getenv("JDTLS_BIN_PATH") or "jdtls"
-	local home_path = os.getenv("HOME") or "~" -- TODO this is not windows friendly
-	lspconfig.jdtls.setup({
-		capabilities=capabilities,
-		on_attach=set_lsp_binds,
-		cmd = {jdtls_bin_path, "-configuration", home_path .. "/.cache/jdtls/config", "-data", home_path .. "/.cache/jdtls/workspace" },
-		workspace = home_path .. "/.cache/jdtls/workspace",
-		-- root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
-	})
+	lspconfig.kotlin_language_server.setup({capabilities=capabilities, on_attach=set_lsp_binds})
+	lspconfig.jdtls.setup({capabilities=capabilities, on_attach=set_lsp_binds})
 	lspconfig.sumneko_lua.setup({
 		capabilites=capabilities,
 		on_attach=set_lsp_binds,
@@ -310,8 +302,6 @@ local init_fn = function(use)
 			}
 		}
 	})
-
-	-- lspconfig.jedi_language_server.setup({capabilites=capabilites})
 end
 
 return require('packer').startup(init_fn)
