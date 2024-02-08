f3088b886f1f87b3fcd13723431ba111257b72e7
alemi <me@alemi.dev>
2024-02-08 15:19:58 +0100

    feat: default wrap on for markdown

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,3 +1,5 @@
 -- quickly mark todo boxes as done or not done
 vim.keymap.set('n', '<leader><space>', "^f[va]c[x]<esc>", {})
 vim.keymap.set('n', '<leader><leader><space>', "^f[va]c[ ]<esc>", {})
+
+vim.o.wrap = true
