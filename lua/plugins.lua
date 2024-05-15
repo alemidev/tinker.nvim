188bb402274be4b8b0917d99012eac987e8689a8
alemi <me@alemi.dev>
2024-05-15 18:49:43 +0200

    fix: ruby_ls -> ruby_lsp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -206,7 +206,7 @@ return {
 			lspconfig.bufls.setup({capabilities=capabilities, on_attach=set_lsp_binds})
 			lspconfig.tsserver.setup({capabilities=capabilities, on_attach=set_lsp_binds})
 			lspconfig.html.setup({capabilities=capabilities, on_attach=set_lsp_binds})
-			lspconfig.ruby_ls.setup({capabilities=capabilities, on_attach=set_lsp_binds})
+			lspconfig.ruby_lsp.setup({capabilities=capabilities, on_attach=set_lsp_binds})
 			lspconfig.elixirls.setup({capabilites=capabilities, on_attach=set_lsp_binds, cmd= {"/usr/bin/elixir-ls"}})
 			lspconfig.gopls.setup({capabilities=capabilities, on_attach=set_lsp_binds})
 			-- lspconfig.rust_analyzer.setup({capabilities=capabilities, on_attach=set_lsp_binds, settings = { ['rust-analyzer'] = { checkOnSave = { command = "clippy"}}}})
